b1013980b6042ee881ea57de3672221887f2ae61
Daria Chepurna <dara.chepurna@gmail.com>
2018-12-18 00:00:04 +0200

    Implement queenThreatens fully

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@ package lesson2.task2
 
 import kotlinx.html.InputType
 import lesson1.task1.sqr
+import kotlin.math.abs
 
 /**
  * Пример
@@ -28,8 +29,7 @@ fun isNumberHappy(number: Int): Boolean =
  * Считать, что ферзи не могут загораживать друг друга.
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
-        (x1 == x2) || (y1 == y2) ||
-
+        (x1 == x2) || (y1 == y2) || abs(x2 - x1) == abs(y2 - y1)
 
 /**
  * Простая
